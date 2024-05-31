5b8fdd0579c16544c1dec69f4d8efa52196c6e3a
INGENIO <ingeniokhalif@gmail.com>
2024-05-31 09:13:55 +0100

    integrated db replaced dummy data by real data

--- api/main.py
+++ api/main.py
@@ -1,37 +1,53 @@
-from typing import Optional
 from fastapi import FastAPI, HTTPException, status, Response
 from pydantic import BaseModel
-from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 server = FastAPI()
+
+# DB connection
+while True:
+    try:
+        conn = psycopg2.connect(
+            database="fastapi_blog",
+            user="postgres",
+            password="postgres",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Connected to DB")
+        break
+    except ConnectionError as e:
+        time.sleep(2)
+        print(e.strerror)
 
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 
-posts = [
-    {"title": "post 1", "content": "content 1", "id": 1},
-    {"title": "post 2", "content": "content 2", "id": 2},
-    {"title": "post 3", "content": "content 3", "id": 3},
-]
+# posts = [
+#     {"title": "post 1", "content": "content 1", "id": 1},
+#     {"title": "post 2", "content": "content 2", "id": 2},
+#     {"title": "post 3", "content": "content 3", "id": 3},
+# ]
 
 
-def findPost(postId: int):
-    for post in posts:
-        if post["id"] == postId:
-            return post
-    return None
+# def findPost(postId: int):
+#     for post in posts:
+#         if post["id"] == postId:
+#             return post
+#     return None
 
 
-def findPostIndex(postId: int):
-    for i, p in enumerate(posts):
-        if p["id"] == postId:
-            return i
-    return None
+# def findPostIndex(postId: int):
+#     for i, p in enumerate(posts):
+#         if p["id"] == postId:
+#             return i
+#     return None
 
 
 @server.get("/")
@@ -41,20 +57,32 @@ def healthCheck():
 
 @server.get("/posts", tags=["Posts"])
 def get_posts():
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
     return {"data": posts}
 
 
 @server.post("/posts", status_code=status.HTTP_201_CREATED, tags=["Posts"])
 def create_post(post: Post):
-    post = post.model_dump()
-    post["id"] = randrange(0, 1e6)
-    posts.append(post)
+    cursor.execute(
+        """INSERT INTO posts(title, content, published) VALUES(%s, %s, %s)
+        RETURNING * """,
+        (post.title, post.content, post.published),
+    )
+
+    post = cursor.fetchone()
+
+    conn.commit()
+
     return {"data": post}
 
 
 @server.get("/posts/{id}", tags=["Posts"])
 def get_post(id: int):
-    post = findPost(id)
+    cursor.execute("SELECT * FROM posts WHERE id = %s", [str(id)])
+
+    post = cursor.fetchone()
+
     if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
@@ -62,30 +90,23 @@ def get_post(id: int):
     return post
 
 
-@server.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT,
-               tags=["Posts"])
+@server.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Posts"])
 def delete_post(id: int):
-    index = findPostIndex(id)
+    post = get_post(id)
 
-    print(index)
+    cursor.execute("DELETE FROM posts WHERE id=%s", [str(post["id"])])
+    conn.commit()
 
-    if index is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
-        )
-    posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@server.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT,
-            tags=["Posts"])
+@server.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Posts"])
 def update_post(id: int, post: Post):
-    index = findPostIndex(id)
+    existingPost = get_post(id)
 
-    if index is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist",
-        )
+    cursor.execute(
+        """UPDATE posts set title=%s, content=%s, published=%s WHERE id=%s""",
+        [post.title, post.content, str(existingPost["id"])],
+    )
 
-    posts[index] = post.model_dump()
+    conn.commit()
